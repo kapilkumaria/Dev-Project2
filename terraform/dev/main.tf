644a31274fd04fa5ed4e1c5701f896efcffe046e
kapil <kapil.kumaria>
2021-02-22 00:47:46 +0000

    modified main.tf in the root module

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -4,22 +4,46 @@ provider "aws" {
 
 
 module "vpc" {
-    source = "../modules/vpc"
-    region = var.region
-
-
-
-
-
-
-
-
+    source              = "../modules/vpc"
+    az_count_v          =
+    az_count_i          =
+    env                 =
+    region              =
+    engineering_domain  =
+    subnet_second_octet =
+    subnet_third_octet  =
+    subnet_identifiers  =
+    peers               =
 }
+
+
+module "eip" {
+    source              = "../modules/eip"
+    az_count_v          = 
+
 
 
 module "sg" {
-    source = "../modules/sg"
-    vpc_id = module.vpc.vpc_id
-    myip = var.myip
-    websg_tag = var.websg_tag  
+    source              = "../modules/sg"
+    websg_tag           =
+    dbsg_tag            =
+    myip                =
+    vpc_id_virginia     = module.vpc.vpc_id_virginia
+    vpc_id_ireland      = module.vpc.vpc_id_ireland
 }
+
+
+module "ec2" {
+    source                  = "../modules/ec2"
+    ami_virginia            =
+    ami_ireland             =
+    instance_type_virginia  =
+    instance_type_ireland   =
+    web_sg                  = module.sg.web_sg_id
+    db_sg                   = module.sg.db_sg_id
+    pri_subnet_virginia     = module.vpc.subnet_ids_private_virginia
+    pri_subnet_ireland      = module.vpc.subnet_ids_private_ireland
+}
+
+
+
