1b2af49ec466b675986bd2f853565f6a8109baef
kapil <kapil.kumaria>
2021-02-26 21:43:57 +0000

    removed ssm-agent executable files

--- terraform/dev/ssm-agent.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo su
-wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.debsudo dpkg -i amazon-ssm-agent.deb
-systemctl enable amazon-ssm-agent
